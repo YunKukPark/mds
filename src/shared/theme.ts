69f3e8d2e91a59d2520bb8bf9d2966822d6ac43b
YunKukPark <a01043340999@gmail.com>
2023-08-06 00:28:04 +0900

    💄Style : Add color theme

---
+++ src/shared/theme.ts
@@ -0,0 +1,51 @@
+const colors = {
+  primary: {
+    main: '#6B68FF',
+    light: '#7B61FF',
+    dark: '#4E4BC9',
+  },
+  secondary: {
+    main: '#26C4F6',
+    light: '#50D5FF',
+    dark: '#29A1C7',
+  },
+  error: {
+    main: '#E94E7C',
+    light: '#F63B73',
+    dark: '#D6295D',
+  },
+  warning: {
+    main: '#F79942',
+    light: '#FF9040',
+    dark: '#DE6B18',
+  },
+  info: {
+    main: '#4FB1F9',
+    light: '#40BAFF',
+    dark: '#2199DC',
+  },
+  success: {
+    main: '#55C861',
+    light: '#36D146',
+    dark: '#1EAA2C',
+  },
+  text: {
+    primary: '#000000',
+    secondary: '#CFCFCF',
+    disabled: 'rgba(256, 256, 256, 0.3)',
+  },
+  others: {
+    background: '#FEFEFE',
+    divider: '#E0E0E0',
+    border: 'rgba(0, 0, 0, 0.23)',
+    backdropOverlay: 'rgba(0, 0, 0, 0.5)',
+    standardInputLine: '#E0E0E0',
+    placeholder: '#CFCFCF',
+  },
+};
+
+const theme = {
+  colors,
+};
+
+export default theme;
