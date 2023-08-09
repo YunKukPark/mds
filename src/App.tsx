a91235ba6629caf2c24ac8b95a0c6f6a5bdb0969
Yunkuk_Park <yun.park@paycrux.co.kr>
2023-08-09 17:50:26 +0900

    config resove path (alias)

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,40 @@
+import {
+  Button,
+  Card,
+  Flex,
+  Grid,
+  Switch,
+  Text,
+  TextArea,
+} from '@radix-ui/themes';
+
+import '@radix-ui/themes/styles.css';
+
 function App() {
-  return <div>App</div>;
+  return (
+    <Card size="2" style={{ maxWidth: 400 }}>
+      <Flex direction="column" gap="3">
+        <Grid gap="1">
+          <Text as="div" weight="bold" size="2" mb="1">
+            Feedback
+          </Text>
+          <TextArea placeholder="Write your feedback…" />
+        </Grid>
+        <Flex asChild justify="between">
+          <label>
+            <Text color="gray" size="2">
+              Attach screenshot?
+            </Text>
+            <Switch size="1" defaultChecked />
+          </label>
+        </Flex>
+        <Grid columns="2" gap="2">
+          <Button variant="surface">Back</Button>
+          <Button>Send</Button>
+        </Grid>
+      </Flex>
+    </Card>
+  );
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
+import { Theme } from '@radix-ui/themes';
+import { styled } from 'styled-components';
+import { flexBox } from '@shared/utils';
+import App from '@src/App';
+
+const S = {
+  Container: styled.div`
+    ${flexBox()}
+  `,
+};
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <S.Container>
+      <Theme accentColor="mint" grayColor="sand" radius="large">
+        <App />
+      </Theme>
+    </S.Container>
   </React.StrictMode>,
 );
