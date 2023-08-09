378966a9863ccc57dbdad7e56dc37d67944ab4ff
Yunkuk Park <53929065+YunKukPark@users.noreply.github.com>
2023-08-09 22:25:09 +0900

    Feature/radix (#1)
    * config resove path (alias)
    
    * 💄style: add color palette
    
    ---------
    
    Co-authored-by: Yunkuk_Park <yun.park@paycrux.co.kr>

---
+++ src/shared/tokens/colors/grayscale.ts
@@ -0,0 +1,29 @@
+export const blackA = {
+  blackA1: 'hsla(0, 0%, 0%, 0.012)',
+  blackA2: 'hsla(0, 0%, 0%, 0.024)',
+  blackA3: 'hsla(0, 0%, 0%, 0.055)',
+  blackA4: 'hsla(0, 0%, 0%, 0.078)',
+  blackA5: 'hsla(0, 0%, 0%, 0.106)',
+  blackA6: 'hsla(0, 0%, 0%, 0.133)',
+  blackA7: 'hsla(0, 0%, 0%, 0.169)',
+  blackA8: 'hsla(0, 0%, 0%, 0.267)',
+  blackA9: 'hsla(0, 0%, 0%, 0.447)',
+  blackA10: 'hsla(0, 0%, 0%, 0.498)',
+  blackA11: 'hsla(0, 0%, 0%, 0.608)',
+  blackA12: 'hsla(0, 0%, 0%, 0.875)',
+};
+
+export const whiteA = {
+  whiteA1: 'hsla(0, 0%, 0%, 0)',
+  whiteA2: 'hsla(0, 0%, 100%, 0.013)',
+  whiteA3: 'hsla(0, 0%, 100%, 0.069)',
+  whiteA4: 'hsla(0, 0%, 100%, 0.104)',
+  whiteA5: 'hsla(0, 0%, 100%, 0.134)',
+  whiteA6: 'hsla(0, 0%, 100%, 0.169)',
+  whiteA7: 'hsla(0, 0%, 100%, 0.216)',
+  whiteA8: 'hsla(0, 0%, 100%, 0.312)',
+  whiteA9: 'hsla(0, 0%, 100%, 0.372)',
+  whiteA10: 'hsla(0, 0%, 100%, 0.455)',
+  whiteA11: 'hsla(0, 0%, 100%, 0.662)',
+  whiteA12: 'hsla(0, 0%, 100%, 0.926)',
+};
