bb602874ec4821c12e52154507011b51ba5d1b81
YunKukPark <a01043340999@gmail.com>
2023-08-19 18:34:43 +0900

    🎨Update : typography 변경

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -5,10 +5,6 @@ import { createGlobalStyle, ThemeProvider } from 'styled-components';
 import { withThemeFromJSXProvider } from '@storybook/addon-styling';
 import { theme, darkTheme } from '../src/shared/theme';
 
-/* TODO: update import for your custom theme configurations */
-// import { lightTheme, darkTheme } from '../path/to/themes';
-
-/* TODO: replace with your own global styles, or remove */
 const GlobalStyles = createGlobalStyle`
     body {
       font-family: "Pretandard", sans-serif;
--- src/shared/tokens/typography.ts
+++ src/shared/tokens/typography.ts
@@ -1,37 +1,28 @@
 export const fontFamily = '"Pretendard Variable" , "Noto Sans KR", sans-serif';
 
 export const fontSizes = {
-  fontSize1: '12px',
-  fontSize2: '14px',
-  fontSize3: '16px',
-  fontSize4: '18px',
-  fontSize5: '20px',
-  fontSize6: '24px',
-  fontSize7: '28px',
-  fontSize8: '36px',
-  fontSize9: '60px',
+  xs: '12px',
+  sm: '14px',
+  base: '16px',
+  lg: '18px',
+  xl: '22px',
+  xxl: '28px',
 };
 
 export const lineHeights = {
-  lineHeight1: '16px',
-  lineHeight2: '20px',
-  lineHeight3: '24px',
-  lineHeight4: '26px',
-  lineHeight5: '28px',
-  lineHeight6: '30px',
-  lineHeight7: '36px',
-  lineHeight8: '40px',
-  lineHeight9: '60px',
+  xs: '16px',
+  sm: '18px',
+  base: '20px',
+  lg: '22px',
+  xl: '26px',
+  xxl: '28px',
 };
 
 export const letterSpacings = {
-  letterSpacing1: '0.0025em',
-  letterSpacing2: '0em',
-  letterSpacing3: '0em',
-  letterSpacing4: '-0.0025em',
-  letterSpacing5: '-0.005em',
-  letterSpacing6: '-0.00625em',
-  letterSpacing7: '-0.0075em',
-  letterSpacing8: '-0.01em',
-  letterSpacing9: '-0.025em',
+  xs: 0,
+  sm: 0,
+  base: 0,
+  lg: 0,
+  xl: 0,
+  xxl: 0,
 };
